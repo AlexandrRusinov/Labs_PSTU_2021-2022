1a46d6b7ddcd32fc1f62be7e574b5328219a7f11
AlexandrRusinov <91492729+AlexandrRusinov@users.noreply.github.com>
2022-03-18 02:07:18 +0500

    Add files via upload

--- 11_2/11_2.cpp
+++ 11_2/11_2.cpp
@@ -1,1 +1,93 @@
+﻿/*
+			21 вариант.
+			Тип информационного поля int.
+			Удалить из списка все элементы с четными информационными полями.			      списки - просто куски память, которые ссылаются на другие куски памяти   данные + указатель на след. кусок (пример: плитки)
+																							  в двунаправленном просто 2 указателя - на след. и пред в одном куске
+*/
+#include <iostream>
+using namespace std;
 
+struct Node
+{
+	int data;
+	Node* ptr_next = nullptr; // pointer to next node(узел)
+	Node* ptr_prev = nullptr;
+};
+
+struct List
+{
+	Node* headnode = nullptr;
+	Node* tailnode = nullptr;
+};
+
+void pushBack(List& list, const int& data) // функция добавления элемента в конец массива
+{
+	Node* newnode = new Node; // создаем-выделяем память под первый узел
+	newnode->data = data; // присваиваем значение узлу
+	if (list.headnode == nullptr) //если список пуст (адрес сразу nullptr)
+	{
+		list.headnode = newnode; // новый узел становится главным узлом
+		list.tailnode = newnode;
+	}
+	else
+	{
+		list.tailnode->ptr_next = newnode;
+		list.tailnode = newnode;
+	}
+}
+
+/*Node* del_element(Node* first, int n)
+{
+Node* p = first;
+for (int i = 0; i < n && p != nullptr; i++)
+{
+if ((p->data)%2 == 0)
+{
+Node* remove = p;
+p=
+
+delete remove;
+}
+p = p->ptr_next;
+// переходим к следующему элементу
+}
+return first; // возвращаем получившийся список
+} */
+
+void change(List& list)
+{
+	
+	Node* currentnode = list.headnode;
+	Node* prednode = list.headnode;
+	while (currentnode != nullptr)
+	{
+		if (currentnode->data % 2 == 0)
+		{
+			prednode->ptr_next = currentnode->ptr_next;
+			prednode = currentnode->ptr_next;
+		}
+		currentnode = currentnode->ptr_next;
+	}	
+}
+int main()
+{
+	setlocale(LC_ALL, "ru");
+	List list; // инициализируем список
+	const int n = 5;
+	int dd;
+	cout << "Заполните список целыми числами" << endl;
+	for (int i = 0; i < n; i++)
+	{
+		cin >> dd;
+		pushBack(list, dd);
+	}
+	Node* first = list.headnode; // указатель на текущий узел
+	//del_element(first, n);
+	change(list);
+	while (first != nullptr)
+	{
+		cout<< first->data << ' '; // выводим данные текущего узла
+		first = first->ptr_next; // переходим к следующему узлу
+	}
+	return 0;
+}
